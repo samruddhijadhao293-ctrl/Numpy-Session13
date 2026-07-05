a64727f05fd2e41f9048eb8a631b5f1bcebea532
samruddhijadhao293-ctrl <samruddhijadhao293@gmail.com>
2026-07-05 23:02:30 +0530

    Added SpecialTask.py

---
+++ SpecialTask.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("student_data.csv")
+
+
+print("First 5 Rows:")
+print(df.head())
+
+print("Last 5 Rows:")
+print(df.tail())
+
+print("Shape:")
+print(df.shape)
+
+print("\nColumns:")
+print(df.columns)
+
+print("\nInfo:")
+df.info()
+
+print("\nSummary:")
+print(df.describe())
+
+print("\nData Types:")
+print(df.dtypes)
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+print("\nUnique Values:")
+print(df.nunique())
+
+print("\nRandom 5 Rows:")
+print(df.sample(5))
+
+print("\nValue Counts:")
+print(df["studytime"].value_counts())
+
